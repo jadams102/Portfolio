88e59ec7ee7d61f30e937898f8ee05d2088244b0
Joel Adams <joeladamsdesign@gmail.com>
2018-05-04 08:47:07 -0700

    add post model

---
+++ Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Portfolio.Models
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string title { get; set; }
+        public DateTime date { get; set; }
+        public string body { get; set; }
+    }
+}
