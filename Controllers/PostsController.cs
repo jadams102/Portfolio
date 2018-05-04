ce7290a0c2cc2a709524a03e730247b77dbd7e1b
Joel Adams <joeladamsdesign@gmail.com>
2018-05-04 10:44:44 -0700

    add index and create posts

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers
+{
+    public class PostsController : Controller
+    {
+        private PortfolioContext db = new PortfolioContext();
+        public IActionResult Index()
+        {
+            List<Post> model = db.Posts.ToList();
+            return View(model);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Post post)
+        {
+            DateTime date = new DateTime();
+            post.date = date;
+            db.Posts.Add(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
