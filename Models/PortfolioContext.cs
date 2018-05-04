f95dc73cab4a8561aed668efa58860442ea339e2
Joel Adams <joeladamsdesign@gmail.com>
2018-05-04 08:54:35 -0700

    add context model

---
+++ Models/PortfolioContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Portfolio.Models
+{
+    public class PortfolioContext
+    {
+        public PortfolioContext()
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(@"Server=localhost;Port=8889;database=joel-portfolio;uid=root;pwd=root;");
+        }
+
+        public PortfolioContext(DbContextOptions<PortfolioContext> options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder)
+        }
+    }
+}
